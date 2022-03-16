6033c02ad4343cf19bd22bd9f7f505c794827b80
Diógenes Ribeiro de Oliveira <diogenes.riboliveira@gmail.com>
2022-03-15 21:53:17 -0300

    commit version - 1.2

--- routeController/linkController.js
+++ routeController/linkController.js
@@ -12,11 +12,9 @@ const createUser = async(req, res) => {
 
     const { username, password, confirmpassword, email, age } = req.body
 
-    if(!email) return res.status(422).json({ msg: 'Insira um email válido!' })
-    if(!password) return res.status(422).json({ msg: 'A senha é obrigatória!' })
-    if(!confirmpassword) return res.status(422).json({ msg: 'Confirme sua senha por favor!' })
-    if(!username) return res.status(422).json({ msg: 'O nome de usuário obrigatório!' })
-    if(!age) return res.status(422).json({ msg: 'Insira sua idade!' })
+    if(!email || !password || !confirmpassword || !username || !age){
+        return res.status(422).json({ msg: 'Preencha todos os campos por favor!' })
+    }
 
     if(password != confirmpassword) return res.status(422).json({ msg: 'As senhas são difierentes!' })
 
@@ -25,23 +23,23 @@ const createUser = async(req, res) => {
     const emailUse = await User.findOne({ email: email })
     const userNameUse = await User.findOne({ username: username })
     
-    if(emailUse) return res.status(422).json({ msg: 'O email digitado já está em uso! Tente outro' })
-    if(userNameUse) return res.status(422).json({ msg: 'O nome de usuário já esta em uso! Tente outro' })
+    if(emailUse){
+        return res.status(422).json({ msg: 'O email digitado já está em uso! Tente outro' })
+    }else if(userNameUse){
+        return res.status(422).json({ msg: 'O nome de usuário já esta em uso! Tente outro' })
+    }
 
     const salt = await bcrypt.genSalt(14)
     const passwordCript = await bcrypt.hash(password, salt)
 
     const user = new User({ username, password: passwordCript, confirmpassword: passwordCript, email, age })
 
-    try {
-        
-        user.save()
-        res.status(200).json({ msg: 'Usuário criado com sucesso! Seja Bem-Vindo ao portal do Usuário' })
-
-    } catch (error) {
-        
+    try{
+       user.save()
+       res.status(200).json({ msgs: 'Usuário criado com sucesso! Seja Bem-Vindo ao portal do usuário!' }) 
+    } catch(error){
         console.log(error)
-
+        res.status(500).json({ msg: '[ERROR - 500] - Ocorreu um erro no servidor! Tente novamente mais tarde' })
     }
 
 }
